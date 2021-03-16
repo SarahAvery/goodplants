ede707338c0a160efc2e194dea65893f5bd26bb0
SarahAvery <70301632+SarahAvery@users.noreply.github.com>
2021-03-16 11:55:37 -0400

    added content

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,30 @@
 console.log("pizza pizza");
+
+//STICKY NAV//
+
+window.onscroll = function () {
+  myFunction();
+};
+
+var nav = document.getElementById("nav");
+
+var sticky = nav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+}
+
+//MOBILE MENU//
+
+const menu = document.querySelector("#menu");
+const menuBtn = document.querySelector("#menu-btn");
+const isOpen = document.querySelector("#nav-mobile");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("open");
+  isOpen.style.transition = "2s, ease in out";
+});
